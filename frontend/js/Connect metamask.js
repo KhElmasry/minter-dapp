6ecebe028711e5ab49ff7eccd216aa7e7ee99de8
ELMA <liv.bettterlife@gmail.com>
2023-01-28 23:19:02 +0200

    test

---
+++ frontend/js/Connect metamask.js
@@ -0,0 +1,18 @@
+const connectButton = document.getElementById("connect-button");
+
+connectButton.addEventListener("click", async function() {
+    if (window.ethereum) {
+        try {
+            // Request account access
+            await window.ethereum.enable();
+            // Connected to Metamask
+            web3 = new Web3(ethereum);
+            console.log("Connected to Metamask");
+        } catch (error) {
+            // User denied account access
+            console.log("User denied account access");
+        }
+    } else {
+        console.log("Metamask not found");
+    }
+});
